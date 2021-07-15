b66b3608e392d87d40bf78fdd4df4d2228512d95
Farhad Mani <frhdmani@gmail.com>
2021-07-15 18:28:31 +0100

    add docstring and small modification to grahm_shmidt module

--- week4/grahm_shmidt.py
+++ week4/grahm_shmidt.py
@@ -1,8 +1,10 @@
-# GRADED FUNCTION
+""""The Gram–Schmidt process takes a finite, linearly independent set of vectors
+S = {v1, …, vk} for k ≤ n and generates an orthogonal set S′ = {u1, …, uk} that spans the
+same k-dimensional subspace of Rn as S. """
 import numpy as np
 import numpy.linalg as la
 
-verySmallNumber = 1e-14 # That's 1×10⁻¹⁴ = 0.00000000000001
+VERY_SMALL_NUMBER = 1e-14 # That's 1×10⁻¹⁴ = 0.00000000000001
 
 # Our first function will perform the Gram-Schmidt procedure for 4 basis vectors.
 # We'll take this list of vectors as the columns of a matrix, A.
@@ -10,39 +12,42 @@ verySmallNumber = 1e-14 # That's 1×10⁻¹⁴ = 0.00000000000001
 # to all the vectors that came before it. Before normalising.
 # Follow the instructions inside the function at each comment.
 # You will be told where to add code to complete the function.
-def gsBasis4(A) :
-    B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
+def gsBasis4(A: np.ndarray) -> np.ndarray :
+    """use GS for four dimensional vectors"""
+    B = np.array(A, dtype=np.float_) # Make B as a copy of A,
     # The zeroth column is easy, since it has no other vectors to make it normal to.
     # All that needs to be done is to normalise it. I.e. divide by its modulus, or norm.
     B[:, 0] = B[:, 0] / la.norm(B[:, 0])
     # For the first column, we need to subtract any overlap with our new zeroth vector.
     B[:, 1] = B[:, 1] - B[:, 1] @ B[:, 0] * B[:, 0]
-    # If there's anything left after that subtraction, then B[:, 1] is linearly independant of B[:, 0]
+    # If there's anything left after that subtraction,
+    # then B[:, 1] is linearly independent of B[:, 0]
     # If this is the case, we can normalise it. Otherwise we'll set that vector to zero.
-    if la.norm(B[:, 1]) > verySmallNumber :
+    if la.norm(B[:, 1]) > VERY_SMALL_NUMBER :
         B[:, 1] = B[:, 1] / la.norm(B[:, 1])
     else :
         B[:, 1] = np.zeros_like(B[:, 1])
     # Now we need to repeat the process for column 2.
     # Insert two lines of code, the first to subtract the overlap with the zeroth vector,
     # and the second to subtract the overlap with the first.
-    B[:, 2] = B[:, 2] - B[:, 2] @ B[:, 0] * B[:, 0]- B[:, 2] @ B[:, 1] * B[:, 1]
-    
+    B[:, 2] = B[:, 2] - (B[:, 2] @ B[:, 0] * B[:, 0]
+        + B[:, 2] @ B[:, 1] * B[:, 1])
+
     # Again we'll need to normalise our new vector.
     # Copy and adapt the normalisation fragment from above to column 2.
-    if la.norm(B[:, 2]) > verySmallNumber :
+    if la.norm(B[:, 2]) > VERY_SMALL_NUMBER :
         B[:, 2] = B[:, 2] / la.norm(B[:, 2])
     else :
         B[:, 2] = np.zeros_like(B[:, 2])
     # Finally, column three:
     # Insert code to subtract the overlap with the first three vectors.
-    B[:, 3] = (B[:, 3] 
+    B[:, 3] = (B[:, 3]
         - B[:, 3] @ B[:, 0] * B[:, 0]
         - B[:, 3] @ B[:, 1] * B[:, 1]
         - B[:, 3] @ B[:, 2] * B[:, 2]
         )
     # Now normalise if possible
-    if la.norm(B[:, 3]) > verySmallNumber :
+    if la.norm(B[:, 3]) > VERY_SMALL_NUMBER :
         B[:, 3] = B[:, 3] / la.norm(B[:, 3])
     else :
         B[:, 3] = np.zeros_like(B[:, 3])
@@ -53,8 +58,9 @@ def gsBasis4(A) :
 # The second part of this exercise will generalise the procedure.
 # Previously, we could only have four vectors, and there was a lot of repeating in the code.
 # We'll use a for-loop here to iterate the process for each vector.
-def gsBasis(A) :
-    B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
+def gsBasis(A: np.ndarray) -> np.ndarray :
+    """GS for n dimensional vectors"""
+    B = np.array(A, dtype=np.float_) # Make B as a copy of A
     # Loop over all vectors, starting with zero, label them with i
     for i in range(B.shape[1]) :
         # Inside that loop, loop over all previous vectors, j, to subtract.
@@ -63,7 +69,7 @@ def gsBasis(A) :
             # you'll need the current vector B[:, i] and a previous vector B[:, j]
             B[:, i] = B[:, i] - B[:,i] @ B[:,j] * B [:,j]
         # Next insert code to do the normalisation test for B[:, i]
-        if la.norm(B[:, i]) > verySmallNumber :
+        if la.norm(B[:, i]) > VERY_SMALL_NUMBER :
             B[:, i] = B[:, i] / la.norm(B[:, i])
         else :
             B[:, i] = np.zeros_like(B[:, i])
@@ -75,5 +81,11 @@ def gsBasis(A) :
 # spanned by a list of vectors.
 # Since each vector is normalised to one, or is zero,
 # the sum of all the norms will be the dimension.
-def dimensions(A) :
+def dimensions(A: np.ndarray) -> float :
+    """
+    This function uses the Gram-schmidt process to calculate the dimension
+    # spanned by a list of vectors.
+    # Since each vector is normalised to one, or is zero,
+    # the sum of all the norms will be the dimension.
+    """
     return np.sum(la.norm(gsBasis(A), axis=0))
